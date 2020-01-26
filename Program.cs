dc81e1eae7e74324749b2acfdf4daef9dd140b8a
Romina <44588411+RominaN@users.noreply.github.com>
2020-01-26 15:38:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyFirstConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.Read();
+		}
+	}
+}
